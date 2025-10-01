4fa1727d4db65e63d3434cd04b73cd807c0ed143
Lilian Geoffrey <chinyere46okeke@gmail.com>
2025-10-01 15:17:10 +0100

    configure remote state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
+terraform {
+backend "s3" {
+  bucket = "terraform-remote-state-abc0282"
+  key    = "terraform.tfstate"
+  region = "eu-central-1"
+}
+}
 provider "aws" {
   region = "eu-central-1"
 }
